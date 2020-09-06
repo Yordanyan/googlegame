543ed7a9670083a6cdc8e2ac4837334c59efec34
Yordanyan <67321987+Yordanyan@users.noreply.github.com>
2020-09-06 21:22:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
-function jump(){
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+   
     if(character.classList == "animate"){return}
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
-}
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=140){
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
+        alert("Game Over score: "+Math.floor(counter/100));
         counter=0;
         block.style.animation = "block 1s infinite linear";
     }else{
@@ -21,3 +24,6 @@ var checkDead = setInterval(function() {
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
 }, 10);
+    }
+}
+e()
